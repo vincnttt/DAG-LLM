2fbfe2ff10ea44edeef60916bfda78427cbfafd3
Vincent Halim <vincenthalim2015@gmail.com>
2024-11-10 22:24:51 +0800

    Added actions and robots configurations

---
+++ configs/actions.py
@@ -0,0 +1,5 @@
+ai2thor_actions = ['GoToObject <robot><object>', 'BackCharge <robot><object>', 'OpenObject <robot><object>', 'CloseObject <robot><object>',
+                   'PickupObject <robot><object>', 'PutObject <robot><object><receptacleObject>', 'PushObject <robot><object>', 'PullObject <robot><object>',
+                   'SwitchOn <robot><object>', 'SwitchOff <robot><object>', 'CleanArea <robot><object>', 'CleanPoint <robot><object>',
+                   'CleanSchedule <robot><object>', 'Conversation <robot>', 'GuideGuest <robot><object>', 'DeliverTask <robot><object>', "ThrowObject <robot><object>"]
+ai2thor_actions = ', '.join(ai2thor_actions)
---
+++ configs/robots.py
@@ -0,0 +1,16 @@
+# List of robots with different configurations
+
+# ALL SKILLS - INF MASS (robot1, robot2, robot3, robot4)
+robot1 = {'name': 'robot1', 'skills': ['GoToObject', 'OpenObject', 'CloseObject', 'BreakObject', 'SliceObject', 'SwitchOn', 'SwitchOff',
+                                       'PickupObject', 'PutObject', 'DropHandObject', 'ThrowObject', 'PushObject', 'PullObject'], 'mass': 100}
+
+robot2 = {'name': 'robot2', 'skills': ['GoToObject', 'OpenObject', 'CloseObject', 'BreakObject', 'SliceObject', 'SwitchOn', 'SwitchOff',
+                                       'PickupObject', 'PutObject', 'DropHandObject', 'ThrowObject', 'PushObject', 'PullObject'], 'mass': 100}
+
+robot3 = {'name': 'robot3', 'skills': ['GoToObject', 'OpenObject', 'CloseObject', 'BreakObject', 'SliceObject', 'SwitchOn', 'SwitchOff',
+                                       'PickupObject', 'PutObject', 'DropHandObject', 'ThrowObject', 'PushObject', 'PullObject'], 'mass': 100}
+
+robot4 = {'name': 'robot4', 'skills': ['GoToObject', 'OpenObject', 'CloseObject', 'BreakObject', 'SliceObject', 'SwitchOn', 'SwitchOff',
+                                       'PickupObject', 'PutObject', 'DropHandObject', 'ThrowObject', 'PushObject', 'PullObject'], 'mass': 100}
+
+robots = [robot1, robot2, robot3, robot4]
